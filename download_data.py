e9b9dbc56062013426b803578d750a93045795eb
Nalu Zou <zonalu@amazon.com>
2025-05-13 14:58:43 -0700

    update

--- download_data.py
+++ download_data.py
@@ -1,9 +1,11 @@
 import os
 import requests
+import tarfile
 
 url = 'https://g-227ca.190ebd.75bc.data.globus.org/ibdmdb/raw/HMP2/MGX/2018-05-04/HSMA33OT.tar'
 
 def main():
+    """
     os.makedirs("genomes", exist_ok=True)
     print ("Sending request")
     response = requests.get(url)
@@ -13,6 +15,10 @@ def main():
         with open("genomes/test", 'wb') as file:
             file.write(response.content)
     print('File downloaded successfully')
+    """
+    tar = tarfile.open("genomes/test", "r:gz")
+    tar.extractall("destination_folder")
+    tar.close()
 
 
 if __name__ == "__main__":
