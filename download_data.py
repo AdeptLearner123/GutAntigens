662b834a4658412f7f331a7a6d3a715f4e3bdf4a
Nalu Zou <zonalu@amazon.com>
2025-05-13 14:54:40 -0700

    update

--- download_data.py
+++ download_data.py
@@ -4,7 +4,7 @@ import requests
 url = 'https://g-227ca.190ebd.75bc.data.globus.org/ibdmdb/raw/HMP2/MGX/2018-05-04/HSMA33OT.tar'
 
 def main():
-    os.mkdir("genomes")
+    os.mkdir("genomes", exist_ok=True)
     print ("Sending request")
     response = requests.get(url)
     print ("Received response")
